7937d2dc82582bcb7dac8cbef74427f805fd8a4c
AnarMuradov <mi82nr231@code.edu.az>
2024-10-29 18:00:56 +0400

    js staretring

---
+++ 10.29-js-intro-task/script.js
@@ -0,0 +1,140 @@
+// let age = +prompt("enter your age")
+// if (age < 125 && age>0) {
+//     if(age>60){
+//     console.log("alirsanda qaqa");
+
+//   } else{
+//     console.log("yo catrmr yasun")
+//   }
+// }else{
+//     console.log("olmemisen sen")
+// }
+// let teref1 = +prompt("I terefi daxil edin")
+// let teref2 = +prompt("II terefi daxil edin")
+// let teref3 = +prompt("III terefi daxil edin")
+// if(teref1==teref2 && teref2==teref3){
+//     console.log("beraberterefli")
+// } else if(teref1==teref2 && teref1!=teref3 || teref1==teref3 && teref1!=teref2 ){
+//     console.log("beraberyanli")
+// }
+// else if(teref1!=teref2 && teref1!=teref3 && teref2!=teref3 ){
+//     console.log("adi ucbucaq")
+// }
+// let eded = +prompt("eded daxil edin");
+// if(eded %2===0){
+//     console.log("cutdu brat")
+// }else{
+//     console.log("texdude qaqa")
+// }
+//switch case
+
+// let yearMonth = +prompt("enter month!");
+
+
+// switch (yearMonth) {
+//   case 1:
+//     console.log(" is January!");
+//     break;
+//   case 2:
+//     console.log(" is February!");
+//     break;
+//   case 3:
+//     console.log(" is Mart!");
+//     break;
+//   case 4:
+//     console.log(" is April!");
+//     break;
+//   case 5:
+//     console.log(" is May!");
+//     break;
+//   case 6:
+//     console.log(" is June!");
+//     break;
+//   case 7:
+//     console.log(" is July!");
+//     break;
+//    case 8:
+//     console.log(" is August!");
+//     break;
+//     case 9:
+//     console.log(" is September!");
+//     break;
+//     case 10:
+//     console.log(" is October!");
+//     break;
+//     case 11:
+//     console.log(" is November!");
+//     break;
+//     case 12:
+//     console.log(" is December!");
+//     break;
+ 
+ 
+ 
+//     default:
+//     console.log(`${weekDay} is not a valid month!`);
+
+//     break;
+// }
+// let eded = +prompt("eded daxil edin");
+// if(eded %15===0){
+//     console.log("bolunur brat")
+// }
+
+// let eded = +prompt("eded daxil edin");
+// if(eded %2===0){
+//      console.log("bolunur brat")
+//  }else{console.log("qaliq hesablandi -qaliq -2")}
+
+// let bal= +prompt("balini yaz baxm")
+// if(bal>90 && bal<=100){
+//     console.log("A almisan ay qaqa")
+// }else if
+//     (bal>80 && bal<=90){
+//         console.log("B almisan ay qaqa")
+//     }
+//     else if
+//     (bal>70 && bal<=80){
+//         console.log("C almisan ay qaqa")
+//     }
+//     else if
+//     (bal>60 && bal<=70){
+//         console.log("A almisan ay qaqa")
+//     }
+//     else if
+//     (bal>0 && bal<=60){
+//         console.log("F almisan aylom")
+//     }
+// let yas= +prompt("yasini yaz bakimm")
+//      if
+//     (yas>64 ){
+//         console.log(" olmusene sen qoica")
+//     }
+//     else if
+//     (yas>17 && yas<=64){
+//         console.log("cavan oglan")
+//     }
+//     else if
+//     (yas>18 && yas<=0){
+//         console.log("Maladoysanem")
+//     }
+
+
+
+// let eded1= +prompt("I reqem");
+// let eded2= +prompt("II reqem");
+// let eded3= +prompt("III reqem");
+
+// let sum =eded1+eded2+eded3;
+// let averageSum= sum/3;
+
+// console.log(`ededi orta ${averageSum}`);
+
+// let name=prompt("enter the user name")
+// let surname=prompt("enter the surname")
+// let fullname=(`${name} ${surname}`)
+// console.log(`${name} ${surname}`);
+
+let emekhaqq= +prompt("ne qede qazansan");
+let verilecek= +prompt("ne qede isty ureyn");
+let muddet= +prompt("ne vaxta qeder brat");
