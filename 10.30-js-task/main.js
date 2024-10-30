fad7043c61c05ea605c7819935f5fb583f4c49d1
AnarMuradov <mi82nr231@code.edu.az>
2024-10-30 19:27:31 +0400

    js task finished

---
+++ 10.30-js-task/main.js
@@ -0,0 +1,122 @@
+// for (let i = 1; i <= 100; i++) {
+//     if (i % 2 === 0) {
+//         console.log(i);
+//     }
+// }
+// let count = 0;
+// for (let i = 1; i <= 100; i += 2) {
+//     count++;
+// }
+// console.log(count);
+
+// let sum = 0;
+
+// for (let i = 1; i <= 100; i++) {
+//     if (i % 5 === 0) {
+//         sum += i;
+//     }
+// }
+
+// console.log(sum);
+// let arr =[12,14,35,56,68,93]
+// let sum=0
+// for (let index = 0; index < array.length; index++) {
+//     sum=sum + arr[index];
+// }
+// console.log(sum);
+
+
+
+// let arr =[12,14,35,56,68,93]
+// for (let i = 0; i < arr.length; i++) {
+//     if (arr[i] % 2 === 1) {
+//        console.log(arr[i])
+//     }
+// }
+
+
+// let arr =[12,14,35,56,68,93]
+// let sum = 0;
+// for (let i = 0; i < arr.length; i++) {
+//     if (arr[i] % 2 === 0) {
+//         sum += arr[i];
+//         console.log(sum)
+//     }
+// }
+
+
+// let arr =[12,14,35,56,68,93]
+// let largest=arr[0]
+// for (let i = 0; i < arr.length; i++) {
+//     if(arr[i]>largest);
+//     largest=arr[i]
+    
+// }
+// console.log(largest);
+    
+// let array =[12,14,35,56,68,93]
+// let little=array[0]
+// let big = array[1]
+// let ferq=0;
+// for (let index = 0; index < array.length; index++) {
+//     if(array[index] <little)
+//         little=array[index]
+//  else if(array[index] >big)
+//         big=array[index]
+//     ferq=big-little
+// }
+// console.log(ferq)
+// let eded =+prompt("eded daxil edin")
+// let fakt=1
+// for (i = 1; i <= eded; i++) {
+//     fakt*=i;
+// }
+
+// alert(fakt);
+// let str = "Lorem ipsum dolor";
+// let count = 0;
+
+// for (let i = 0; i < srt.length; i++) {
+//    if(str[i].toLowerCase()==="1")
+//     c++
+// }
+
+// console.log(count);
+
+
+// let eded=+prompt("ededi daxil edin")
+
+
+// for (let i = 2; i < array.length; i++) {
+//     if (eded %i === 0) {
+        
+//     }
+  
+// }
+// const scores=[60, 75, 88, 75,93,45]
+// let avarage;
+// let total= 0;
+
+// for (let i = 0; i < scores.length; i++) {
+//     total += scores[i];
+//     avarage=total / scores.length
+// }
+// console.log(avarage);
+
+
+// const scores  =[12,14,35,56,68,93]
+// let largest=scores[0]
+// for (let i = 0; i < scores.length; i++) {
+//     if(scores[i]>largest);
+//         largest=scores[i]
+        
+// }
+// console.log(largest);
+const scores  =[12,14,35,56,68,93]
+let minimal=scores[0]
+for (let i = 0; i > scores.length; i++) {
+    if(scores[i] > minimal);
+        minimal=scores[i]
+        
+}
+console.log(minimal);
