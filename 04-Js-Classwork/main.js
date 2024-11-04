c4b7de434f583e2af669835cd8e71f86989662ca
AnarMuradov <mi82nr231@code.edu.az>
2024-11-04 19:17:02 +0400

    finishhh

---
+++ 04-Js-Classwork/main.js
@@ -0,0 +1,38 @@
+
+
+// function sozIndex(sentence, word) {
+//     const words = sentence.split(" ");
+//     for (let i = 0; i < sentence.length; i++) {
+//         if (words[i]==word) {
+//             return i;
+//         }
+        
+//     }
+// }
+// console.log(sozIndex("Lorem ipsum dolor sit, amet consectetur adipisicing elit. Nam, dolores.","ipsum"));
+
+
+// const sentence = "Lorem ipsum dolor sit, amet consectetur adipisicing elit. Nam, dolores.";
+// function uzunSoz(sentence) {
+   
+//     const words = sentence.split(' ');
+    
+
+//     let longestWord = '';
+//   for (let i = 0; i < words.length; i++) {
+//         if(words[i].length > longestWord.length){
+//             longestWord = words[i]
+//         }
+    
+//   }
+
+//     return longestWord;
+// }
+// console.log(uzunSoz(sentence));
+
+
+
+
+
+
+
