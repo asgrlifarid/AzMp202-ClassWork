fc4ee5f192425d42aad9541c440913abe604a37f
AnarMuradov <mi82nr231@code.edu.az>
2024-11-07 18:58:08 +0400

    js da  css

---
+++ 11-7-JS-TASK/main.js
@@ -0,0 +1,19 @@
+// const divBox=document.createElement('div')
+// const parag=document.createElement('h1')
+// parag.textContent='Azmp202'
+// parag.style.display="flex"
+// parag.style.justifyContent="center"
+// parag.style.alignItems="center"
+// parag.style.color="white"
+// divBox.append(parag)
+// divBox.style.width="250px"
+// divBox.style.height="250px"
+// divBox.style.border="1px solid red"
+// divBox.style.borderRadius="15px"
+// divBox.style.backgroundColor="green"
+
+
+
+// document.body.append(divBox)
+
+
