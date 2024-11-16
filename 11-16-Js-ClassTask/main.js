fe41e48be36aebe5a422649fd72cacc5bfd39951
asgrlifarid <faridaskerli2@gmail.com>
2024-11-16 18:31:16 +0400

    finitto

---
+++ 11-16-Js-ClassTask/main.js
@@ -0,0 +1,21 @@
+// const input =document.querySelector("#timeInput");
+// const geriSay =document.querySelector("#geriSayim")
+// const startBtn =document.querySelector("#startButton")
+// const h1=document.querySelector(".h1")
+
+
+
+// startBtn.addEventListener("click", function () {
+ 
+// let count=input.valueAsNumber;
+// h1.textContent = count;
+//     const intervalId= setInterval(() => {
+//     --count;
+//     h1.textContent = count;
+//   if(count===0){
+//     clearInterval (intervalId);
+//     h1.textContent = "finish";
+//   }
+//   },1000);
+// });
+import { users } from "./data.js";
